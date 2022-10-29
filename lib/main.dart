170a5c6386c7dc359306b69ed62c696a8a4f92dd
blendthink <admin@blendthink.dev>
2022-10-30 02:48:20 +0900

    Use appName in dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  const appName = String.fromEnvironment('appName');
   runApp(
     const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('appName = '),
+          child: Text('appName = $appName'),
         ),
       ),
     ),
